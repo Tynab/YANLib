bbb766430d07e47a5720bda6d4d3338c510a2434
Yami An <yamiannephilim@gmail.com>
2023-09-05 00:55:08 +0700

    completed sample

--- lib/YANLib/YANNum.Int.cs
+++ lib/YANLib/YANNum.Int.cs
@@ -1,4 +1,6 @@
-﻿namespace YANLib;
+﻿using static System.Linq.Enumerable;
+
+namespace YANLib;
 
 public static partial class YANNum
 {
@@ -15,48 +17,15 @@ public static partial class YANNum
         }
     }
 
-    public static IEnumerable<int> ToInt<T>(this IEnumerable<T> nums) where T : struct
-    {
-        if (nums.IsEmptyOrNull())
-        {
-            yield break;
-        }
-
-        foreach (var num in nums)
-        {
-            yield return num.ToInt();
-        }
-    }
+    public static IEnumerable<int>? ToInt<T>(this IEnumerable<T> nums) where T : struct => nums.IsEmptyOrNull() ? default : nums.Select(n => n.ToInt());
 
     public static int ToInt(this string str) => int.TryParse(str, out var num) ? num : default;
 
-    public static IEnumerable<int> ToInt(this IEnumerable<string> strs)
-    {
-        if (strs.IsEmptyOrNull())
-        {
-            yield break;
-        }
-
-        foreach (var num in strs)
-        {
-            yield return num.ToInt();
-        }
-    }
+    public static IEnumerable<int>? ToInt(this IEnumerable<string> strs) => strs.IsEmptyOrNull() ? default : strs.Select(s => s.ToInt());
 
     public static int ToInt<T>(this string str, T dfltVal) where T : struct => int.TryParse(str, out var num) ? num : dfltVal.ToInt();
 
-    public static IEnumerable<int> ToInt<T>(this IEnumerable<string> strs, T dfltVal) where T : struct
-    {
-        if (strs.IsEmptyOrNull())
-        {
-            yield break;
-        }
-
-        foreach (var num in strs)
-        {
-            yield return num.ToInt(dfltVal);
-        }
-    }
+    public static IEnumerable<int>? ToInt<T>(this IEnumerable<string> strs, T dfltVal) where T : struct => strs.IsEmptyOrNull() ? default : strs.Select(s => s.ToInt(dfltVal));
 
     public static int GenerateRandomInt<T1, T2>(T1 min, T2 max) where T1 : struct where T2 : struct
     {
@@ -66,13 +35,7 @@ public static partial class YANNum
         return minValue > maxValue ? default : new Random().Next(minValue, maxValue);
     }
 
-    public static IEnumerable<int> GenerateRandomInts<T1, T2, T>(T1 min, T2 max, T size) where T1 : struct where T2 : struct where T : struct
-    {
-        for (var i = 0ul; i < size.ToUlong(); i++)
-        {
-            yield return GenerateRandomInt(min, max);
-        }
-    }
+    public static IEnumerable<int> GenerateRandomInts<T1, T2, T>(T1 min, T2 max, T size) where T1 : struct where T2 : struct where T : struct => Range(0, size.ToUint().ToInt()).Select(i => GenerateRandomInt(min, max));
 
     public static int GenerateRandomInt() => GenerateRandomInt(int.MinValue, int.MaxValue);
 
--- lib/YANLib/YANNum.Long.Nullable.cs
+++ lib/YANLib/YANNum.Long.Nullable.cs
@@ -1,4 +1,6 @@
-﻿namespace YANLib;
+﻿using static System.Linq.Enumerable;
+
+namespace YANLib;
 
 public static partial class YANNum
 {
@@ -15,87 +17,29 @@ public static partial class YANNum
         }
     }
 
-    public static IEnumerable<long> ToLong<T>(this IEnumerable<T?> nums) where T : struct
-    {
-        if (nums.IsEmptyOrNull())
-        {
-            yield break;
-        }
-
-        foreach (var num in nums)
-        {
-            yield return num.ToLong();
-        }
-    }
+    public static IEnumerable<long>? ToLong<T>(this IEnumerable<T?> nums) where T : struct => nums.IsEmptyOrNull() ? default : nums.Select(n => n.ToLong());
 
     public static long ToLong<T>(this string str, T? dfltVal) where T : struct => dfltVal.HasValue ? str.ToLong(dfltVal.Value) : default;
 
-    public static IEnumerable<long> ToLong<T>(this IEnumerable<string> strs, T? dfltVal) where T : struct
-    {
-        if (strs.IsEmptyOrNull())
-        {
-            yield break;
-        }
-
-        foreach (var num in strs)
-        {
-            yield return num.ToLong(dfltVal);
-        }
-    }
+    public static IEnumerable<long>? ToLong<T>(this IEnumerable<string> strs, T? dfltVal) where T : struct => strs.IsEmptyOrNull() ? default : strs.Select(s => s.ToLong(dfltVal));
 
     public static long GenerateRandomLong<T1, T2>(T1? min, T2 max) where T1 : struct where T2 : struct => min.HasValue ? GenerateRandomLong(min.Value, max) : default;
 
-    public static IEnumerable<long> GenerateRandomLongs<T1, T2, T>(T1? min, T2 max, T size) where T1 : struct where T2 : struct where T : struct
-    {
-        for (var i = 0ul; i < size.ToUlong(); i++)
-        {
-            yield return GenerateRandomLong(min, max);
-        }
-    }
+    public static IEnumerable<long> GenerateRandomLongs<T1, T2, T>(T1? min, T2 max, T size) where T1 : struct where T2 : struct where T : struct => Range(0, size.ToUint().ToInt()).Select(i => GenerateRandomLong(min, max));
 
-    public static IEnumerable<long> GenerateRandomLongs<T1, T2, T>(T1? min, T2 max, T? size) where T1 : struct where T2 : struct where T : struct
-    {
-        for (var i = 0ul; i < size.ToUlong(); i++)
-        {
-            yield return GenerateRandomLong(min, max);
-        }
-    }
+    public static IEnumerable<long> GenerateRandomLongs<T1, T2, T>(T1? min, T2 max, T? size) where T1 : struct where T2 : struct where T : struct => Range(0, size.ToUint().ToInt()).Select(i => GenerateRandomLong(min, max));
 
     public static long GenerateRandomLong<T1, T2>(T1 min, T2? max) where T1 : struct where T2 : struct => max.HasValue ? GenerateRandomLong(min, max.Value) : default;
 
-    public static IEnumerable<long> GenerateRandomLongs<T1, T2, T>(T1 min, T2? max, T size) where T1 : struct where T2 : struct where T : struct
-    {
-        for (var i = 0ul; i < size.ToUlong(); i++)
-        {
-            yield return GenerateRandomLong(min, max);
-        }
-    }
+    public static IEnumerable<long> GenerateRandomLongs<T1, T2, T>(T1 min, T2? max, T size) where T1 : struct where T2 : struct where T : struct => Range(0, size.ToUint().ToInt()).Select(i => GenerateRandomLong(min, max));
 
-    public static IEnumerable<long> GenerateRandomLongs<T1, T2, T>(T1 min, T2? max, T? size) where T1 : struct where T2 : struct where T : struct
-    {
-        for (var i = 0ul; i < size.ToUlong(); i++)
-        {
-            yield return GenerateRandomLong(min, max);
-        }
-    }
+    public static IEnumerable<long> GenerateRandomLongs<T1, T2, T>(T1 min, T2? max, T? size) where T1 : struct where T2 : struct where T : struct => Range(0, size.ToUint().ToInt()).Select(i => GenerateRandomLong(min, max));
 
     public static long GenerateRandomLong<T1, T2>(T1? min, T2? max) where T1 : struct where T2 : struct => min.HasValue ? GenerateRandomLong(min.Value, max) : default;
 
-    public static IEnumerable<long> GenerateRandomLongs<T1, T2, T>(T1? min, T2? max, T size) where T1 : struct where T2 : struct where T : struct
-    {
-        for (var i = 0ul; i < size.ToUlong(); i++)
-        {
-            yield return GenerateRandomLong(min, max);
-        }
-    }
+    public static IEnumerable<long> GenerateRandomLongs<T1, T2, T>(T1? min, T2? max, T size) where T1 : struct where T2 : struct where T : struct => Range(0, size.ToUint().ToInt()).Select(i => GenerateRandomLong(min, max));
 
-    public static IEnumerable<long> GenerateRandomLongs<T1, T2, T>(T1? min, T2? max, T? size) where T1 : struct where T2 : struct where T : struct
-    {
-        for (var i = 0ul; i < size.ToUlong(); i++)
-        {
-            yield return GenerateRandomLong(min, max);
-        }
-    }
+    public static IEnumerable<long> GenerateRandomLongs<T1, T2, T>(T1? min, T2? max, T? size) where T1 : struct where T2 : struct where T : struct => Range(0, size.ToUint().ToInt()).Select(i => GenerateRandomLong(min, max));
 
     public static long GenerateRandomLong<T>(T? max) where T : struct => max.HasValue ? GenerateRandomLong(long.MinValue, max.Value) : default;
 }
