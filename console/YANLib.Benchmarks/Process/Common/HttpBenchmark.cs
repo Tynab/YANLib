2a0c39e4e42f38a4dc5a9dbb81080cd7b22bdbc4
Yami An <truongan.nguyendang@hoozing.vn>
2023-09-07 20:37:53 +0700

    http benchmark

---
+++ console/YANLib.Benchmarks/Process/Common/HttpBenchmark.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace YANLib.Benchmarks.Process.Common;
+
+public class HttpBenchmark
+{
+    private HttpClient? _httpClient;
+
+    [Params(1_000, 10_000, 100_000, 1_000_000)]
+    public int Size { get; set; }
+
+    [GlobalSetup]
+    public void Setup() => _httpClient = new HttpClient();
+
+    [Benchmark]
+    public async ValueTask<string> FetchUrl() => await _httpClient!.GetStringAsync("https://dev-api-support.hoozing.com/api/hoozing-support/agency-property-assignments/get-by-agent-id?agentId=0c4dec3a-c421-4a51-b94d-201fa9125256");
+}
