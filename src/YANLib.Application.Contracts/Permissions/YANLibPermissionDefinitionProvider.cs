914b8df77879329b5feb30feb35780a4fcd0a933
Yami An <yamiannephilim@gmail.com>
2024-07-30 22:03:44 +0700

    240730

--- src/YANLib.Application.Contracts/Permissions/YANLibPermissionDefinitionProvider.cs
+++ src/YANLib.Application.Contracts/Permissions/YANLibPermissionDefinitionProvider.cs
@@ -1,7 +1,4 @@
 ﻿using Volo.Abp.Authorization.Permissions;
-using Volo.Abp.Localization;
-using YANLib.Localization;
-using static Volo.Abp.Localization.LocalizableString;
 using static YANLib.Permissions.YANLibPermissions;
 
 namespace YANLib.Permissions;
--- src/YANLib.Application/YANLibAccessorService.cs
+++ src/YANLib.Application/YANLibAccessorService.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using Volo.Abp.Security.Claims;
-using YANLib.Core;
 
 namespace YANLib;
 
