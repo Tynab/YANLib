322d61dbc3b2ba0a7e53c68276dfa0de1cfef2bb
Yami An <yamiannephilim@gmail.com>
2025-02-22 01:05:26 +0700

    Update ConcurrentBenchmark.cs

--- console/YANLib.Benchmarks/Process/Common/ConcurrentBenchmark.cs
+++ console/YANLib.Benchmarks/Process/Common/ConcurrentBenchmark.cs
@@ -1,14 +1,16 @@
 ﻿using BenchmarkDotNet.Attributes;
+using static BenchmarkDotNet.Configs.BenchmarkLogicalGroupRule;
 using static System.Threading.Tasks.Task;
 
 namespace YANLib.Benchmarks.Process.Common;
 
+[SimpleJob, GroupBenchmarksBy(ByCategory), CategoriesColumn]
 public class ConcurrentBenchmark
 {
     [Params(1_000, 10_000, 100_000, 1_000_000)]
     public int Iterations { get; set; }
 
-    [Benchmark(Baseline = true)]
+    [Benchmark(Baseline = true), BenchmarkCategory("Synchronous")]
     public async Task Synchronous_Task()
     {
         var sum = 0;
@@ -19,7 +21,7 @@ public class ConcurrentBenchmark
         }
     }
 
-    [Benchmark]
+    [Benchmark, BenchmarkCategory("Synchronous")]
     public async Task Synchronous_ValueTask()
     {
         var sum = 0;
@@ -30,43 +32,47 @@ public class ConcurrentBenchmark
         }
     }
 
-    //[Benchmark]
-    //public async Task Asynchronous_Task()
-    //{
-    //    var sum = 0;
-
-    //    for (var i = 0; i < Iterations; i++)
-    //    {
-    //        sum += await GetAsyncTask();
-    //    }
-    //}
-
-    //[Benchmark]
-    //public async Task Asynchronous_ValueTask()
-    //{
-    //    var sum = 0;
-
-    //    for (var i = 0; i < Iterations; i++)
-    //    {
-    //        sum += await GetAsyncValueTask();
-    //    }
-    //}
-
     private static Task<int> GetSyncTask() => FromResult(1);
 
     private static ValueTask<int> GetSyncValueTask() => new(1);
 
-    //private static async Task<int> GetAsyncTask()
-    //{
-    //    await Delay(1);
+    [Benchmark(Baseline = true), BenchmarkCategory("Asynchronous")]
+    public async Task Asynchronous_Task()
+    {
+        var sum = 0;
 
-    //    return 1;
-    //}
+        for (var i = 0; i < Iterations; i++)
+        {
+            sum += await GetAsyncTask();
+        }
+    }
 
-    //private static async ValueTask<int> GetAsyncValueTask()
-    //{
-    //    await Delay(1);
+    [Benchmark, BenchmarkCategory("Asynchronous")]
+    public async Task Asynchronous_ValueTask()
+    {
+        var sum = 0;
 
-    //    return 1;
-    //}
+        for (var i = 0; i < Iterations; i++)
+        {
+            sum += await GetAsyncValueTask();
+        }
+    }
+
+    private static async Task<int> GetAsyncTask()
+    {
+        var result = 1 + 1;
+
+        await Yield();
+
+        return result;
+    }
+
+    private static async ValueTask<int> GetAsyncValueTask()
+    {
+        var result = 1 + 1;
+
+        await Yield();
+
+        return result;
+    }
 }
