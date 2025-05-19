bd80d3b22dd4e0d969a8efe1c1ab6791d4f38e4d
Yami An <yamiannephilim@gmail.com>
2025-05-19 19:18:03 +0700

    id gen init

---
+++ lib/YANLib.Implementation/Snowflake/IdGenerator.Constructor.cs
@@ -0,0 +1,42 @@
+﻿namespace YANLib.Implementation.Snowflake;
+
+public partial class IdGenerator
+{
+    #region Fields
+
+    private long _lastTimestamp = -1;
+    private readonly object _lock = new();
+
+    #endregion
+
+    #region Constructors
+
+    protected internal IdGenerator(long workerId, long datacenterId, long sequence = default)
+    {
+        if (workerId is < 0 or > MAX_WORKER_ID)
+        {
+            throw new ArgumentException($"Worker ID must be between 0 and {MAX_WORKER_ID}, but got {workerId}.", nameof(workerId));
+        }
+
+        if (datacenterId is < 0 or > MAX_DATACENTER_ID)
+        {
+            throw new ArgumentException($"Datacenter ID must be between 0 and {MAX_DATACENTER_ID}, but got {datacenterId}.", nameof(datacenterId));
+        }
+
+        WorkerId = workerId;
+        DatacenterId = datacenterId;
+        Sequence = sequence;
+    }
+
+    #endregion
+
+    #region Properties
+
+    public long WorkerId { get; protected set; }
+
+    public long DatacenterId { get; protected set; }
+
+    public long Sequence { get; internal set; }
+
+    #endregion
+}
---
+++ lib/YANLib/Snowflake/IdGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace YANLib.Snowflake;
+
+public partial class IdGenerator(long workerId, long datacenterId, long sequence = 0) : Implementation.Snowflake.IdGenerator(workerId, datacenterId, sequence)
+{
+    [DebuggerHidden]
+    [DebuggerStepThrough]
+    internal long NextId(long timestampEpoch = TIMESTAMP_EPOCH) => NextIdImplement(timestampEpoch);
+
+    [DebuggerHidden]
+    [DebuggerStepThrough]
+    internal string NextIdAlphabetic(long timestampEpoch = TIMESTAMP_EPOCH) => NextIdAlphabeticImplement(timestampEpoch);
+
+    [DebuggerHidden]
+    [DebuggerStepThrough]
+    internal string NextIdAlphanumeric(long timestampEpoch = TIMESTAMP_EPOCH) => NextIdAlphanumericImplement(timestampEpoch);
+
+    [DebuggerHidden]
+    [DebuggerStepThrough]
+    internal static (DateTime Timestamp, long WorkerId, long DatacenterId) ExtractIdComponents(long id, long timestampEpoch = TIMESTAMP_EPOCH, long sequence = default)
+        => ExtractIdComponentsImplement(id, timestampEpoch, sequence);
+
+    [DebuggerHidden]
+    [DebuggerStepThrough]
+    internal static (DateTime Timestamp, long WorkerId, long DatacenterId) ExtractIdAlphabeticComponents(string id, long timestampEpoch = TIMESTAMP_EPOCH, long sequence = default)
+        => ExtractIdAlphabeticComponentsImplement(id, timestampEpoch, sequence);
+
+    [DebuggerHidden]
+    [DebuggerStepThrough]
+    internal static (DateTime Timestamp, long WorkerId, long DatacenterId) ExtractIdAlphanumericComponents(string id, long timestampEpoch = TIMESTAMP_EPOCH, long sequence = default)
+        => ExtractIdAlphanumericComponentsImplement(id, timestampEpoch, sequence);
+}
